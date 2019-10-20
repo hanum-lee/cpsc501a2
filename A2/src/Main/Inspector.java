d4084959bbafa411101d2a1f74cc0b29e71cacd4
hanum-lee <chococloudy@gmail.com>
2019-10-20 00:23:17 -0600

    Recursive function for inspecting super class added

--- A2/src/Main/Inspector.java
+++ A2/src/Main/Inspector.java
@@ -1,4 +1,5 @@
 package Main;
+//import java.lang.*;
 
 public class Inspector {
 
@@ -8,6 +9,25 @@ public class Inspector {
     }
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+        System.out.println(c);
+        recurSuper(c);
+
+        //Class superC = c.getSuperclass();
+        //System.out.println(superC);
+    }
+
+    private void recurSuper(Class c){
+        Class superC = c.getSuperclass();
+        if(superC == null){
+
+        }else{
+            System.out.println(superC);
+            recurSuper(superC);
+        }
+    }
+
+    private void recurInter(Class c){
+
     }
 
 }
--- A2/src/Main/Main.java
+++ A2/src/Main/Main.java
@@ -2,7 +2,16 @@ package Main;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 	// write your code here
+        System.out.println("Tests");
+        Object testObj = new ClassA();
+        Class objClass = testObj.getClass();
+        System.out.println(objClass);
+        ClassA classA = new ClassA();
+        Inspector inspec = new Inspector();
+        inspec.inspect(new ClassA(), false);
+
+
     }
 }
