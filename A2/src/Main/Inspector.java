090f2c450a045198d02c600caa40040dfd3f9657
hanum-lee <chococloudy@gmail.com>
2019-10-24 14:51:50 -0600

    Getting fields for method and constructor finished

--- A2/src/Main/Inspector.java
+++ A2/src/Main/Inspector.java
@@ -4,6 +4,7 @@ package Main;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public class Inspector {
 
@@ -14,31 +15,56 @@ public class Inspector {
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) throws NoSuchMethodException, IllegalAccessException {
         System.out.println(c);
-        recurSuper(c);
+        System.out.println("########Super Class########");
+        recurSuper(c,depth+1);
+
+        System.out.println("#########Interfaces########");
+        recurInter(c,depth);
+
+        System.out.println("########Constructor########");
         for(int i = 0; i < c.getDeclaredConstructors().length; i++){
             //System.out.println(c.getDeclaredConstructors()[i]);
             Constructor con = c.getDeclaredConstructors()[i];
-            System.out.println(con.getName());
-            System.out.println(con.getDeclaringClass());
+            System.out.println("Name: " +con.getName());
+            //System.out.println(con.getDeclaringClass());
+            System.out.println("Parameter Types: ");
             for(int j = 0; j < con.getParameterCount(); j++){
-                System.out.println(con.getParameterTypes()[j]);
+                System.out.println("\t" + con.getParameterTypes()[j]);
             }
-            System.out.println(con.getModifiers());
+            System.out.println("Modifiers: " + con.getModifiers());
 
         }
-        System.out.println("Methods");
+
+
+        System.out.println("########Methods########");
         for(int i = 0; i < c.getDeclaredMethods().length ; i ++){
             Method met = c.getDeclaredMethods()[i];
-            System.out.println(met);
+            //System.out.println(met);
+            System.out.println("Name: " + met.getName());
+            Class[] metException = met.getExceptionTypes();
+            System.out.println("Exceptions: ");
+            for (Class excp: metException) {
+                System.out.println("\t" + excp);
+            }
+            Class[] metParam = met.getParameterTypes();
+            System.out.println("Parameters: ");
+            for(Class param:metParam){
+                System.out.println("\t"+param.getName());
+            }
+            Class metReturn = met.getReturnType();
+            System.out.println("Return type: " + metReturn.getName());
+            int metModint = met.getModifiers();
+            System.out.println("Modifiers: " + Modifier.toString(metModint));
+
+
         }
 
 //        Constructor cons = c.getDeclaredConstructor();
 //        System.out.println("constructor:" + cons);
-        System.out.println("Interfaces");
-        Class[] inter = c.getInterfaces();
-        System.out.println(inter[0]);
 
-        System.out.println("Fields");
+        //System.out.println(inter[0]);
+
+        System.out.println("########Fields########");
         for(int i = 0; i < c.getDeclaredFields().length; i++){
             Field f = c.getDeclaredFields()[i];
             f.setAccessible(true);
@@ -47,20 +73,29 @@ public class Inspector {
         }
         //Class superC = c.getSuperclass();
         //System.out.println(superC);
+//        String testSt = new String("test");
+//        Class testcs = testSt.getClass();
+//        recurSuper(testcs);
     }
 
-    private void recurSuper(Class c){
+    private void recurSuper(Class c, int depth){
         Class superC = c.getSuperclass();
-        if(superC == null){
 
-        }else{
+        if(superC != null){
+            System.out.println("Super Class of " + c.getName());
             System.out.println(superC);
-            recurSuper(superC);
+            recurSuper(superC, depth+1);
+        }else{
+
         }
     }
 
-    private void recurInter(Class c){
-
+    private void recurInter(Class c, int depth){
+        Class[] inter = c.getInterfaces();
+        for(int i = 0; i < inter.length; i++){
+            System.out.println(inter[i]);
+            recurInter(inter[i],depth+1);
+        }
     }
 
 }
--- A2/src/Main/Main.java
+++ A2/src/Main/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 	// write your code here
-        System.out.println("Tests");
+        //System.out.println("Tests");
         Object testObj = new ClassA();
         Class objClass = testObj.getClass();
-        System.out.println(objClass);
+        //System.out.println(objClass);
         ClassA classA = new ClassA();
         Inspector inspec = new Inspector();
-        inspec.inspect(new ClassB(), false);
+        inspec.inspect(new ClassA(), false);
 
 
     }
